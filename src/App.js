1a387f03c6f0a30294a00e1def80e2526645cb56
Ayush9040 <ayushaggarwal9040@gmail.com>
2023-07-01 08:14:07 +0530

    portfolio first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Navbar from './components/Navbar';
+import Portfolio from './pages/portfolio/Portfolio';
+import Contact from './pages/contact/Contact';
+import About from './pages/about/About';
+import Themes from './components/Themes';
+// import './index.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     <Navbar />
+     <Themes />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='portfolio' element={<Portfolio />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
